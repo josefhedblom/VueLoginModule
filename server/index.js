5969ca6eb85fe0f7e184700715bbbfc7c05e6bf6
Josef Hedblom <hedblomjosef@gmail.com>
2021-03-12 12:44:15 +0100

    Add config to index.js

---
+++ server/index.js
@@ -0,0 +1,19 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config();
+}
+
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+connectDB();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
