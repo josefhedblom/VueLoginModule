06b2c9733e88165941972966f400f0133d97445e
Josef Hedblom <hedblomjosef@gmail.com>
2021-03-12 13:02:37 +0100

    Add database config

---
+++ server/config/db.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+const dbString = process.env.DB;
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(dbString{
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log('Connected to database....');
+    } catch (error) {
+        console.log(error.message);
+        console.log('Error');
+        process.exit(1);
+    }
+};
+
+module.exports = connectDB;
